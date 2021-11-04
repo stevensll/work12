f1d18aaf3a03940be522839920edccbd45bb5414
Steven <deltaorb637@gmail.com>
2021-11-03 23:56:01 -0400

    need to add input

--- main.c
+++ main.c
@@ -7,18 +7,17 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <dirent.h>
-#define PATH "./"
+#define PATH "."
 
-void print_info(struct dirent *dir, long long *p){
-    struct stat buf;
-    int err = stat(dir->d_name, &buf);
-    if(err == -1){
-        printf("Error getting info of directory %s\n%s\n", dir->d_name, strerror(errno));
-    }
+void print_info(struct dirent *d){
+    struct stat *stt;
 
 }
 
 int main(){
+
+    
+
 
     DIR *dir = opendir(PATH);
     if(!dir){
@@ -26,13 +25,31 @@ int main(){
         return 0;
     }
 
-    struct dirent *entry = readdir(dir);
-
-    while(entry){
-        print_info()
-        entry = readdir(dir);
+    struct dirent *entry;
+    struct stat temp;
+    printf("Driectories:\n");
+    long int size = 0;
+    //printing directories
+    while(entry=readdir(dir)){
+        if (entry->d_type == 4){
+            stat(entry->d_name, &temp);
+            printf("\t%s\tSize:%ld Bytes\n", entry->d_name, temp.st_size);
+            size+=temp.st_size;
+        }
     }
+    
+    rewinddir(dir);
+
+    printf("Files:\n");
+    //printing files
+    while(entry=readdir(dir)){
+        if (entry->d_type == 8){
+            stat(entry->d_name, &temp);
+            printf("\t%s\tSize:%ld Bytes\n", entry->d_name, temp.st_size);
+            size+=temp.st_size;
+        }
+    }
+    printf("Total directory size: %ld Bytes\n", size);
     closedir(dir);
-    // closedir(d);
     return 0;
 }
